782cc9eb0e4940f2d0a1ae9fbd57e2d9a7f15aa8
mwehner2022 <mwehner@megans-MacBook-Pro.local>
2023-02-06 11:51:00 -0700

    cleaner

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,92 +1,49 @@
-
-R version 4.2.2 (2022-10-31) -- "Innocent and Trusting"
-Copyright (C) 2022 The R Foundation for Statistical Computing
-Platform: x86_64-apple-darwin17.0 (64-bit)
-
-R is free software and comes with ABSOLUTELY NO WARRANTY.
-You are welcome to redistribute it under certain conditions.
-Type 'license()' or 'licence()' for distribution details.
-
-Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-> setwd("/Users/mwehner/Desktop/BootCamp/MechaCar_Statistical_Analysis")
-> library(tidyverse)
-── Attaching packages ──────────────────────────────────────────────────────────────── tidyverse 1.3.2 ──
-✔ ggplot2 3.4.0     ✔ purrr   1.0.1
-✔ tibble  3.1.8     ✔ dplyr   1.1.0
-✔ tidyr   1.3.0     ✔ stringr 1.5.0
-✔ readr   2.1.3     ✔ forcats 1.0.0
-── Conflicts ─────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
-✖ dplyr::filter() masks stats::filter()
-✖ dplyr::lag()    masks stats::lag()
-> library(dplyr)
-> install.packages("dplyr")
-Error in install.packages : Updating loaded packages
-
-Restarting R session...
-
-> install.packages("dplyr")
-trying URL 'https://cran.rstudio.com/bin/macosx/contrib/4.2/dplyr_1.1.0.tgz'
-Content type 'application/x-gzip' length 1554483 bytes (1.5 MB)
-==================================================
-  downloaded 1.5 MB
-
-
-The downloaded binary packages are in
-/var/folders/23/zl9yqn5x13dgspnwvqxkrvz40000gp/T//Rtmpx0gB4o/downloaded_packages
-> library(tidyverse)
-── Attaching packages ──────────────────────────────────────────────────────────────── tidyverse 1.3.2 ──
-✔ ggplot2 3.4.0     ✔ purrr   1.0.1
-✔ tibble  3.1.8     ✔ dplyr   1.1.0
-✔ tidyr   1.3.0     ✔ stringr 1.5.0
-✔ readr   2.1.3     ✔ forcats 1.0.0
-── Conflicts ─────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
-✖ dplyr::filter() masks stats::filter()
-✖ dplyr::lag()    masks stats::lag()
-> MechaCarData <- read.csv(file='Starter_Code/MechaCar_mpg.csv', check.names=F,stringsAsFactors = F) #import
-> View(MechaCarData)
-> View(MechaCarData)
-> lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData) # generate multiple linear regression model
-
-Call:
-  lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
-       ground_clearance + AWD, data = MechaCarData)
-
-Coefficients:
-  (Intercept)    vehicle_length    vehicle_weight     spoiler_angle  ground_clearance  
--1.040e+02         6.267e+00         1.245e-03         6.877e-02         3.546e+00  
-AWD  
--3.411e+00  
-
-> summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData)) #get summary stats
-
-Call:
-  lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
-       ground_clearance + AWD, data = MechaCarData)
-
-Residuals:
-  Min       1Q   Median       3Q      Max 
--19.4701  -4.4994  -0.0692   5.4433  18.5849 
-
-Coefficients:
-  Estimate Std. Error t value Pr(>|t|)    
-(Intercept)      -1.040e+02  1.585e+01  -6.559 5.08e-08 ***
-  vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
-  vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
-spoiler_angle     6.877e-02  6.653e-02   1.034   0.3069    
-ground_clearance  3.546e+00  5.412e-01   6.551 5.21e-08 ***
-  AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
----
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-Residual standard error: 8.774 on 44 degrees of freedom
-Multiple R-squared:  0.7149,	Adjusted R-squared:  0.6825 
-F-statistic: 22.07 on 5 and 44 DF,  p-value: 5.35e-11
+> #import dplyr
+  > library(dplyr)
+> 
+  > #read csv
+  > MechaCarData <- read.csv(file='Starter_Code/MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
+  > head(MechaCarData)
+  vehicle_length vehicle_weight spoiler_angle ground_clearance AWD      mpg
+  1       14.69710       6407.946      48.78998         14.64098   1 49.04918
+  2       12.53421       5182.081      90.00000         14.36668   1 36.76606
+  3       20.00000       8337.981      78.63232         12.25371   0 80.00000
+  4       13.42849       9419.671      55.93903         12.98936   1 18.94149
+  5       15.44998       3772.667      26.12816         15.10396   1 63.82457
+  6       14.45357       7286.595      30.58568         13.10695   0 48.54268
+  > # generate multiple linear regression model
+    > lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData) 
+  
+  Call:
+    lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+         ground_clearance + AWD, data = MechaCarData)
+  
+  Coefficients:
+    (Intercept)    vehicle_length    vehicle_weight     spoiler_angle  ground_clearance               AWD  
+  -1.040e+02         6.267e+00         1.245e-03         6.877e-02         3.546e+00        -3.411e+00  
+  
+  > #get summary stats
+    > summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData)) 
+  
+  Call:
+    lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+         ground_clearance + AWD, data = MechaCarData)
+  
+  Residuals:
+    Min       1Q   Median       3Q      Max 
+  -19.4701  -4.4994  -0.0692   5.4433  18.5849 
+  
+  Coefficients:
+    Estimate Std. Error t value Pr(>|t|)    
+  (Intercept)      -1.040e+02  1.585e+01  -6.559 5.08e-08 ***
+    vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
+    vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
+  spoiler_angle     6.877e-02  6.653e-02   1.034   0.3069    
+  ground_clearance  3.546e+00  5.412e-01   6.551 5.21e-08 ***
+    AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
+  ---
+    Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+  
+  Residual standard error: 8.774 on 44 degrees of freedom
+  Multiple R-squared:  0.7149,	Adjusted R-squared:  0.6825 
+  F-statistic: 22.07 on 5 and 44 DF,  p-value: 5.35e-11
